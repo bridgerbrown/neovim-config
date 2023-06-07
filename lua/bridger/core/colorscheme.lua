7479133546bdd32ffd901f1fc044c8fbb69cbfdf
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-06-06 21:15:55 -0700

    noirbuddy miami-nights theme change

--- lua/bridger/core/colorscheme.lua
+++ lua/bridger/core/colorscheme.lua
@@ -1,4 +1,4 @@
-local status, _ = pcall(vim.cmd, "colorscheme nvim-noirbuddy")
+local status, _ = pcall(vim.cmd, "colorscheme noirbuddy")
 if not status then
 	print("Colorscheme not found!")
 	return
