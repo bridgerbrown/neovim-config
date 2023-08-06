87d1294349d7d45a2fd8bdafd323992eecae7417
Bridger Brown <106130443+bridgerbrown@users.noreply.github.com>
2023-08-05 22:04:18 -0700

    Custom Lualine for Kanagawa

--- lua/bridger/plugins/lualine.lua
+++ lua/bridger/plugins/lualine.lua
@@ -3,10 +3,44 @@ if not status then
 	return
 end
 
+
+local colors = {
+  sumiInk0 = "#16161D",
+  fujiWhite = "#DCD7BA",
+  springGreen = "#98BB6C",
+  crystalBlue = "#7E9CD8",
+  springViolet1 = "#938AA9",
+  springViolet2 = "#9CABCA",
+  sakuraPink = "#D27E99",
+}
+
+local function icon()
+  return [[]]
+end
+
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
+		theme = {
+      normal = {
+        a = { fg = colors.sumiInk0, bg = colors.springGreen },
+        b = { fg = colors.springViolet1, bg = colors.sumiInk0 },
+        c = { fg = colors.fujiWhite, bg = colors.sumiInk0 },
+        z = { fg = colors.fujiWhite, bg = colors.sumiInk0 },
+      },
+      insert = {
+        a = { fg = colors.sumiInk0, bg = colors.fujiWhite },
+        z = { fg = colors.fujiWhite, bg = colors.sumiInk0 },
+      },
+      visual = {
+        a = { fg = colors.sumiInk0, bg = colors.springViolet2 },
+        z = { fg = colors.fujiWhite, bg = colors.sumiInk0 },
+      },
+      replace = {
+        a = { fg = colors.sumiInk0, bg = colors.sakuraPink },
+        z = { fg = colors.fujiWhite, bg = colors.sumiInk0 },
+      },
+    },
 		component_separators = { left = "", right = "" },
 		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
@@ -23,7 +57,7 @@ require("lualine").setup({
 		},
 	},
 	sections = {
-		lualine_a = { { "mode", icons_enabled = true, padding = 2 } },
+		lualine_a = { {icon}, {"mode", icons_enabled = true, padding = 2 } },
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { "filename" },
 		lualine_x = { "filetype" },
